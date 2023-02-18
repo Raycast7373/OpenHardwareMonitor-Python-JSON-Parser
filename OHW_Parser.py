06fea30ef6ae94dbaeb619a797d2b95ac6eb2ec3
Raycast_ <bluemagicalwolf@gmail.com>
2023-02-18 13:26:07 +0100

    Add files via upload

--- OHW_Parser.py
+++ OHW_Parser.py
@@ -5,6 +5,7 @@ import time
 HostIP = '127.0.0.1'
 HostPort = '8123'
 loop = True
+ShowIDs = False # set this to True to see the ids (prints it when you use the PrintAllInfo function) you would need to use to get the specific sensor using the PrintData or PrintSpecificSensor function
 
 def PrintData(sensorid1, sensorid2):
     count = 0
@@ -16,6 +17,14 @@ def PrintData(sensorid1, sensorid2):
         data_name = data['Text']
         print(data_name, ":", "min:", data_min, "max:", data_max, "current:", data_current)
         count+=1
+
+def PrintSpecificSensor(sensorid1, sensorid2, sensorid3):
+    data = OHW_DICT['Children'][0]['Children'][sensorid1]['Children'][sensorid2]['Children'][sensorid3]
+    data_min = data['Min']
+    data_max = data['Max']
+    data_current = data['Value']
+    data_name = data['Text']
+    print(data_name, ":", "min:", data_min, "max:", data_max, "current:", data_current)
 
 def PrintAllInfo():
     counta=0
@@ -35,6 +44,9 @@ def PrintAllInfo():
                 data_max = data['Max']
                 data_current = data['Value']
                 data_name = data['Text']
+                if ShowIDs == True:
+                    print("")
+                    print("Sensor ID thing:", "Categorty ID:", counta, "Subcategory ID:", countb, "Data/Sensor ID:", countc)
                 print(data_name, ":", "min:", data_min, "max:", data_max, "current:", data_current)
                 countc+=1
             countc=0
@@ -56,6 +68,12 @@ if loop == False:
     OHW_ADDRESS = "http://"+HostIP+":"+HostPort+"/data.json"
     OHW_DICT = json.loads(requests.get(OHW_ADDRESS).text)
     PrintAllInfo() # prints all data from openhardwaremonitor
+
+#print specific sensor
+#first value is the device (1 for example is the cpu in most cases)
+#seccond value is the specific sensor category (cpu usage, cpu temperature, cpu power usage)
+#third value is the specific sensor to target (cpu core 1 for example)
+#PrintSpecificSensor(1, 1, 1) #prints the temperatur of the first cpu core
 
 #CPU
 #BLCK
